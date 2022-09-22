97ab9eda4ab98aee31adfab10b9476eaa0ec33a4
eastfiles <feyswallmkota92@gmail.com>
2022-09-22 16:47:37 +0300

    solve conflicts

--- comparison_op.php
+++ comparison_op.php
@@ -8,18 +8,9 @@
 		<?php
 		$marks = 70;
 		 
-<<<<<<< HEAD
-		if( $marks > 50 or $marks < 50){
-			echo" go down ";
-		}
-	
-		  
-		 
-=======
 		 if ($marks > 60 or $marks !=70) {
 		 	echo "thats great";
 		 }
->>>>>>> 9d49489c15a68092be071bdfefed668cab3b451e
 
 		?>
 	</body>
