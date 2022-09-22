9d49489c15a68092be071bdfefed668cab3b451e
eastfiles <feyswallmkota92@gmail.com>
2022-09-22 15:36:18 +0300

    done desc

--- welcome/index.php
+++ welcome/index.php
@@ -1,6 +1,23 @@
+
 <!DOCTYPE html>
+<!-- 
+    !DOCTYPE html
+    Is the code that describe what type ofk html document is this, 
+    and this specify that this is a html5 document.
+ -->
+
 <html lang="en">
+<!-- 
+    <html lang="en">
+    This shows the start of html document. Then lang="en" describes that the
+    primary language ofk this dockument is english.        
+-->
     <head>
+<!-- 
+    head
+    This is then part where extra information about the page is stored.
+    Example: style, tittle and links to fonts and jacvascript.
+-->
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,6 +32,10 @@
                         <input type="text" name="fname">
                         <?php
                             if( isset($_GET['error']) ){
+                                /** isset() 
+                                 * Is a function in php that we use to show wether the variable
+                                 * exits or does not exist in the file.
+                                 */
                                 if( $_GET['error'] == 'first'){
                                      echo '
                                         <div style="margin-bottom: 10px; color: red">
